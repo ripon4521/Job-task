9a9cfca28d79e2cbae2c5d8dbf4f30045faf4568
ripon4521 <riponakondo4521@gmail.com>
2024-06-13 02:02:03 +0600

    create responsive popular card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import PopularCard from "./components/PopularCard";
 
 function App() {
   return (
     <div className="bg-gray-100">
       <Navbar />
       <Banner/>
+      <PopularCard/>
     </div>
   );
 }
