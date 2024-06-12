964a5820154f7c7666ff1dc77d50e18ae5c4a0c6
ripon4521 <riponakondo4521@gmail.com>
2024-06-13 01:29:35 +0600

    Create Responsive navbar

--- src/components/Dropdawon.jsx
+++ src/components/Dropdawon.jsx
@@ -13,11 +13,11 @@ const Dropdawon = () => {
       <button 
         id="dropdownDefaultButton" 
         onClick={toggleDropdown} 
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
+        className=" bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800" 
         type="button"
       >
-        Dropdown button 
-        <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+     MENU
+        <svg className="w-2.5 h-2.5 ms-3 text-red-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
         </svg>
       </button>
@@ -25,16 +25,16 @@ const Dropdawon = () => {
         <div id="dropdown" className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute right-0 mt-2">
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:text-red-500">Dashboard</a>
             </li>
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white  hover:text-red-500">Settings</a>
             </li>
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white  hover:text-red-500" >Earnings</a>
             </li>
             <li>
-              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+              <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white  hover:text-red-500">Sign out</a>
             </li>
           </ul>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import Dropdawon from "./Dropdawon";
+import { FaRegUser } from "react-icons/fa";
+
+
 
 const Navbar = () => {
     return (
-        <div className="container mx-auto">
-            <div>
-            <h1>Pti.</h1>
-            <label className="input input-bordered flex items-center gap-2">
+        <div className="container mx-auto px-2 md:px-0">
+            <div className="flex justify-between py-5 items-center">
+            <h1 className="text-2xl font-inter font-bold drop-shadow-md">pti.</h1>
+   <div className="flex md:gap-10 items-center gap-3">
+   <label className="input h-10 md:w-96 w-40 flex items-center gap-2 ">
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5 opacity-70 text-red-500 "><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
   <input type="text" className="grow" placeholder="Search" />
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+ 
 </label>
 <Dropdawon/>
+   </div>
+
+<p className="text-2xl bg-orange-600 p-2 rounded-full text-white hidden md:block"><FaRegUser/></p>
             </div>
            
         </div>
